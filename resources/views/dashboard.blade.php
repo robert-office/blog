df3fc6c710e385cdd2fb8160cb9aa723f7ab6b17
robert-office <robertuillians123@gmail.com>
2022-07-05 12:11:42 -0300

    chore: added Post Controller e all others resources

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,15 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
                 </div>
+
+
+
+
+
+
             </div>
         </div>
     </div>
+
+
 </x-app-layout>
